9d97066822ce1870f00955704dc1537827f5d683
footballnft <85647902+footballnft@users.noreply.github.com>
2023-06-01 07:32:50 +0100

    Error-41

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { styled, StyledEngineProvider } from '@mui/system';
+import { Box, styled, StyledEngineProvider } from '@mui/system';
 import { AppBar, Toolbar, Typography } from '@mui/material';
 import Link from 'next/link';
 import SearchBar from './SearchBar';
@@ -25,22 +25,36 @@ const SiteLogo = styled('img')({
     <StyledEngineProvider injectFirst>
       <CustomNavigationBar>
         <NavigationBarContent>
-          <Link href="/">
-            <a>
-              <SiteLogo src="/public/Logo.png" alt="Site Logo" />
-            </a>
-          </Link>
-          <Typography variant="h6">TallyFundMe</Typography>
-          <Link href="/Campaigns">
-            <a>Campaigns</a>
-          </Link>
-          <Link href="/HowItWorks">
-            <a>How It Works</a>
-          </Link>
-          <SearchBar onSearch={handleSearch} />
-          <Link href="/Login">
-            <a>Login</a>
-          </Link>
+            <Link href="/">
+                <a>
+                  <SiteLogo src="/public/Logo.png" alt="Site Logo" />
+                </a>
+            </Link>
+                  <Box>
+                      <nav> 
+                        <ul role="menubar">
+                          <li role="none">
+                          <Typography variant="h6">TallyFundMe</Typography>
+                          </li>
+                          <li role="none">
+                          <Link href="/Campaigns">
+                            <a>Campaigns</a>
+                          </Link>
+                          </li>
+                          <li role="none">
+                          <Link href="/HowItWorks">
+                            <a>How It Works</a>
+                          </Link>
+                          </li>
+                          </ul>  
+                      </nav>
+                  </Box> 
+                  <Box>
+                          <SearchBar onSearch={handleSearch} />
+                          <Link href="/Login">
+                            <a>Login</a>
+                          </Link>
+                  </Box>  
         </NavigationBarContent>
       </CustomNavigationBar>
     </StyledEngineProvider>
